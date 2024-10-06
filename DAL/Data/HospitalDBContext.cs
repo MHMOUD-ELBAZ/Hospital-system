5caafe9c21dc673551925749f82f5b25355d3ce0
MHMOUD-ELBAZ <elbaz8536@gmail.com>
2024-10-06 20:38:52 +0300

    Add project files.

---
+++ DAL/Data/HospitalDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public class HospitalDBContext : IdentityDbContext <AppUser>
+    {
+        public HospitalDBContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public HospitalDBContext(): base() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server = .; Database = hospital; Trusted_Connection = true; TrustServerCertificate = true; MultipleActiveResultSets = true;");
+        }
+    }
+}
---
+++ Hospital_Project_01/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hospital_Project_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
